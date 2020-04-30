77c826bfd34453359c7f10a9c08b876182500e6e
Hashir Ali <ali.hashir051@gmail.com>
2020-04-29 21:00:17 -0400

    Commit v 0.0.1
    Added "wolves" that dash at the player when in range
    Need to fix dashing for multiple enemies.

---
+++ Assets/Scripts/AI Scripts/BasePackBehavController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePackBehavController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField]
+    float DashSpeed = 0;
+
+    [SerializeField]
+    float MinAngle = 0;
+
+    public bool Attacked = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public void Attack(Transform playerPos)
+    {
+        // Face player and dash towards them dealing damage
+        if (Vector3.Dot(transform.forward, (playerPos.position - transform.position).normalized) < MinAngle)
+        {
+            transform.LookAt(playerPos);
+        }
+        else
+        {
+
+            rb.AddForce(transform.forward * DashSpeed, ForceMode.Impulse);
+            Attacked = true;
+            StartCoroutine("ResetAttack");
+        }
+
+
+    }
+
+
+
+
+    IEnumerator ResetAttack()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Attacked = false;
+
+    }
+
+}
---
+++ Assets/Scripts/AI Scripts/BasePackController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePackController : MonoBehaviour
+{
+    GameObject Player;
+
+    GameObject[] Animals;
+
+    [SerializeField]
+    float MovementSpeed = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Animals = GameObject.FindGameObjectsWithTag("Pack");
+
+    }
+
+    private void Update()
+    {
+        CycleAttacks();
+    }
+
+    void CycleAttacks()
+    {
+        // When x number of enemies are in range, attack one by one
+        foreach (GameObject animal in Animals)
+        {
+            if (Vector3.Distance(Player.transform.position, animal.transform.position) <= 8 && !animal.GetComponent<BasePackBehavController>().Attacked)
+            {
+                animal.GetComponent<BasePackBehavController>().Attack(Player.transform);
+            }
+            else
+            {
+                animal.transform.position = Vector3.MoveTowards(animal.transform.position, Player.transform.position, MovementSpeed * Time.deltaTime);
+            }
+
+        }
+
+    }
+
+}
